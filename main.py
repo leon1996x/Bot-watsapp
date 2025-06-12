121309f2c6bd3df99db49547d04385daeb90b136
leon1996x <leva.alexa1996@gmail.com>
2025-06-13 00:04:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from state_machine import (
 
 app = FastAPI()
 
-WAZZUP_TOKEN = "4e68fe2f438140b0ba531c114509b1e9"
+WAZZUP_TOKEN = ""
 WEBHOOK_URL = "https://bot-watsapp-y7e8.onrender.com/webhook/wazzup"
 CHANNEL_ID = "fe817b21-47a7-a021-9b5200c4cf29"
 
